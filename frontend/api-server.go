dc4a73456bd7de1fb40ff609d05f56c105aaaff5
Tarek Ziesak <tarek.ziesak@mailbox.org>
2018-05-28 11:53:04 +0200

    simplified code

--- frontend/api-server.go
+++ frontend/api-server.go
@@ -10,40 +10,24 @@ import (
 
 func main() {
 	e := echo.New()
-
 	g := e.Group("/api")
-
 	g.Use(middleware.BasicAuth(func(u, p string, c echo.Context) (bool, error) {
 		return u == "username" && p == "password", nil
 	}))
-
-	g.GET("/heroes", func(c echo.Context) (err error) {
-		return sendJSON(c, "data/heros.json")
-	})
-
-	g.GET("/adventures", func(c echo.Context) (err error) {
-		return sendJSON(c, "data/adventures.json")
-	})
-
-	g.GET("/adventure/:id", func(c echo.Context) (err error) {
-		return sendJSON(c, "data/adventure"+c.Param("id")+".json")
-	})
-
-	g.GET("/templates", func(c echo.Context) (err error) {
-		return sendJSON(c, "data/templates.json")
-	})
-
-	g.GET("/template/:id", func(c echo.Context) (err error) {
-		return sendJSON(c, "data/template"+c.Param("id")+".json")
-	})
-
+	g.GET("/heroes", sendJSON("heros"))
+	g.GET("/adventures", sendJSON("adventures"))
+	g.GET("/adventure/:id", sendJSON("adventure"))
+	g.GET("/templates", sendJSON("templates"))
+	g.GET("/template/:id", sendJSON("template"))
 	e.Logger.Fatal(e.Start("127.0.0.1:9090"))
 }
 
-func sendJSON(c echo.Context, path string) error {
-	bytes, err := ioutil.ReadFile(path)
-	if len(bytes) == 0 || err != nil {
-		return err
+func sendJSON(file string) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		bytes, err := ioutil.ReadFile("data/" + file + c.Param("id") + ".json")
+		if len(bytes) == 0 || err != nil {
+			return err
+		}
+		return c.JSONBlob(http.StatusOK, bytes)
 	}
-	return c.JSONBlob(http.StatusOK, bytes)
 }
