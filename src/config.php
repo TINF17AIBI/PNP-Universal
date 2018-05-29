fa063ec4e202610c607f151ae48f17c0ec3aab39
Tarek Ziesak <tarek.ziesak@mailbox.org>
2018-05-29 16:59:16 +0200

    switched the whole project to php and server side rendering

---
+++ src/config.php
@@ -0,0 +1,5 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set("display_errors", "on");
+    $conn = new PDO('mysql:host=ENTER_HOST_HERE;dbname=ENTER_DATABASE_NAME_HERE', 'ENTER_USERNAME_HERE', 'ENTER_PASSWORD_HERE');
+?>
