cb405d27318ad7f1dc7cd43043fa76c3824d8609
Zernerus <38104558+Zernerus@users.noreply.github.com>
2018-06-09 13:35:19 +0200

    Added controlpanel

---
+++ src/public/pages/controlpanel.php
@@ -0,0 +1,19 @@
+<?php require_once("navbar.php"); ?>
+
+		<form>
+		  <div class="form-group">
+			<label for="InputPasswordOld">Old Password</label>
+			<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter your old Password">
+	      </div>
+		  <div class="form-group">
+			<label for="InputPasswordNew">New Password</label>
+			<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter your new Password">
+	      </div>
+		  <div class="form-group">
+			<label for="InputPasswordNewAgain">New Password</label>
+			<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter your new Password(again,we know it's annoying)">
+	      </div>
+		</form>
+		 </div>
+		  </body>
+</html>
